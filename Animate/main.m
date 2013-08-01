57457fdcc31cd7aa361c3fc50978cf06b77e8d66
pandazheng <543828315@qq.com>
2013-08-01 07:06:52 -0700

    Initial Commit

---
+++ Animate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Animate
+//
+//  Created by panda zheng on 13-8-1.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
